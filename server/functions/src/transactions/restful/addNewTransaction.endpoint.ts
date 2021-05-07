92bf3c0843bda4ea425c5e5e13c42cea4474d489
Blessing Hirwa <hirwablessing7@gmail.com>
2021-05-07 19:07:04 +0200

    Add transaction time

--- server/functions/src/transactions/restful/addNewTransaction.endpoint.ts
+++ server/functions/src/transactions/restful/addNewTransaction.endpoint.ts
@@ -1,5 +1,5 @@
-/* eslint-disable linebreak-style */
 import { Request, Response } from "express";
+import firebase from "firebase";
 import { Endpoint, RequestType } from "firebase-backend";
 import { db } from "../../config/firebase";
 
@@ -14,8 +14,6 @@ export default new Endpoint(
     const initialBalance = request.body["initialBalance"];
     const newBalance = request.body["newBalance"];
 
-    console.log(cardUUid, cardOwner, initialBalance);
-
     try {
       const entry = db.collection("tapAndPay").doc();
       const entryObject = {
@@ -25,6 +23,7 @@ export default new Endpoint(
         transport_fare: transportFare,
         initial_balance: initialBalance,
         new_balance: newBalance,
+        transaction_time: firebase.firestore.FieldValue.serverTimestamp(),
       };
 
       entry.set(entryObject);
@@ -37,11 +36,5 @@ export default new Endpoint(
     } catch (error) {
       response.status(500).send(error.message);
     }
-
-    // return response.status(201).send({
-    //   uuid: cardUUid,
-    //   owner: cardOwner,
-    //   balance: initialBalance,
-    // });
   }
 );
